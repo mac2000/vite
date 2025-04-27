8ece963bc5fe3693c5be3385ce0b0bba7d35cb0d
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2025-04-27 20:43:54 +0300

    tw4

--- App.tsx
+++ App.tsx
@@ -4,6 +4,10 @@ export function App() {
   return (
     <div>
       <h1>Hello Vite</h1>
+      <div className="flex gap-4">
+        <input type="text" />
+        <button className="btn">hello</button>
+      </div>
     </div>
   );
 }
